e13e96e34ac58aaf4dd302b717e38d67d41c863c
imix108 <136431946+imix108@users.noreply.github.com>
2024-02-14 16:42:39 +0300

    1.1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,57 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) {}
 
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open. Person enters the house.");
+    } else {
+      console.log("The door is closed. First, you must open the door.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("Door is open.");
+    } else {
+      console.log("Door is closed.");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
